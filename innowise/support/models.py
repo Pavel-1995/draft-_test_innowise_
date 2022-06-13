baf59278b1dd750a5bafb292da26443283c4e5eb
Pavel Aliaksei <lexa.kun.11@mail.ru>
2022-06-13 12:05:08 +0300

    create models

--- innowise/support/models.py
+++ innowise/support/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Ticket(models.Model):
+    class Status(models.Model):
+        statu_s = [
+            (1, "В обработке"),
+            (2, "Обработан"),
+            (3, "Закрыт")
+        ]
+    text_ticke = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    name_user = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.PositiveSmallIntegerField(('status'), choices=Status.statu_s, blank=False, default=1)
+
+    def __str__(self):
+        return self.text_ticke
+
+
+class Message(models.Model):
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    ticket_id = models.ForeignKey(Ticket, on_delete=models.PROTECT)
+    name_user = models.ForeignKey(User, on_delete=models.PROTECT)
+    text_answer = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.text_answer
